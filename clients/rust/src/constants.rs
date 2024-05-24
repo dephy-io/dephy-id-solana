ceb15710dbd39f663fb4b1155ae063316b3ec104
Jun Jiang <jasl9187@hotmail.com>
2024-05-24 17:23:19 +0800

    Update client constants

--- clients/rust/src/constants.rs
+++ clients/rust/src/constants.rs
@@ -1,7 +1,6 @@
 // Should keep in sync with program's constants
 
 pub const PROGRAM_PDA_SEED_PREFIX: &[u8; 8] = b"DePHY_ID";
-pub const VENDOR_MINT_SEED_PREFIX: &[u8; 15] = b"DePHY_ID-VENDOR";
 pub const PRODUCT_MINT_SEED_PREFIX: &[u8; 16] = b"DePHY_ID-PRODUCT";
 pub const DEVICE_MINT_SEED_PREFIX: &[u8; 15] = b"DePHY_ID-DEVICE";
 
