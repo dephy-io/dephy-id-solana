a841fcdaa85b81d9c56576d304d44cc7fabf0880
gaowhen <gaowhen.com@gmail.com>
2024-07-01 10:04:55 +0800

    generated client

--- clients/rust/src/generated/instructions/create_activated_device.rs
+++ clients/rust/src/generated/instructions/create_activated_device.rs
@@ -91,8 +91,10 @@ impl CreateActivatedDevice {
             self.owner, false,
         ));
         accounts.extend_from_slice(remaining_accounts);
-        let mut data = borsh::to_vec(&CreateActivatedDeviceInstructionData::new()).unwrap();
-        let mut args = borsh::to_vec(&args).unwrap();
+        let mut data = CreateActivatedDeviceInstructionData::new()
+            .try_to_vec()
+            .unwrap();
+        let mut args = args.try_to_vec().unwrap();
         data.append(&mut args);
 
         solana_program::instruction::Instruction {
@@ -478,8 +480,10 @@ impl<'a, 'b> CreateActivatedDeviceCpi<'a, 'b> {
                 is_writable: remaining_account.2,
             })
         });
-        let mut data = borsh::to_vec(&CreateActivatedDeviceInstructionData::new()).unwrap();
-        let mut args = borsh::to_vec(&self.__args).unwrap();
+        let mut data = CreateActivatedDeviceInstructionData::new()
+            .try_to_vec()
+            .unwrap();
+        let mut args = self.__args.try_to_vec().unwrap();
         data.append(&mut args);
 
         let instruction = solana_program::instruction::Instruction {
